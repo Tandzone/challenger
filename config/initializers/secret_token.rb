f957da18108d6aa09a865449c1c74d6847ff7faf
Tanjona ubuntu <tanjonatsiry@gmail.com>
2015-07-10 14:40:27 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenger::Application.config.secret_key_base = '0bed3aff0975008c86a038e1e04ae08e9f2f13bdb1e1132e07e5c69202743fb7cca066020c6bc48e000fb96fd29c190ca8be3bd65065bc114b696b512a57a356'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Challenger::Application.config.session_store :cookie_store, key: '_challenger_session'
